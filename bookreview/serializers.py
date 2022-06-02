7a74aed466389252adf851b0f429f1a9f9c2ae0f
Abhishant Singh <abhishantsingh05@gmail.com>
2022-06-03 00:24:43 +0530

    added SerializerMethodField for custom property search url

--- bookreview/serializers.py
+++ bookreview/serializers.py
@@ -3,11 +3,23 @@ from .models import Author
 from .models import Book
 
  
- 
+class BookSerializer(serializers.ModelSerializer):
+    """
+    Serializing all the Books
+    """
+    search_url = serializers.SerializerMethodField('get_search_url')
+    class Meta:
+        model = Book
+        fields = ('id', 'title', 'isbn','author','search_url')
+    def get_search_url(self, obj):
+        return "http://www.isbnsearch.org/isbn/{}".format(obj.isbn)
+
 class AuthorSerializer(serializers.ModelSerializer):
     """
     Serializing all the Authors
     """
+    books = BookSerializer(read_only=True, many=True, source="book_set")
     class Meta:
         model = Author
-        fields = ('id'
+        fields = ('id', 'first_name', 'last_name', 'books')
+
